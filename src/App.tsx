4f1932cd8bdc3cec9c6db9db7b4389e6e06cf1ea
Franck Farcy <franck.farcy@gmail.com>
2025-07-17 19:36:40 +0200

    Fix image paths with GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import CinqueQueensPage from "./CinqueQueensPage";
 function App() {
   return (
     <CinqueQueensPage
-      coverSrc="/cinque_terre_bw.png"
-      groupSrc="/group_photo_bw.png"
-      pdfHref="/Cinque_Queens_Weekend_Revised.pdf"
+      coverSrc="/cinque-queens-weekend/cinque_terre_bw.png"
+      groupSrc="/cinque-queens-weekend/group_photo_bw.png"
+      pdfHref="/cinque-queens-weekend/Cinque_Queens_Weekend_Revised.pdf"
     />
   );
 }
