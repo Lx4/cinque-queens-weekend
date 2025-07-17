876380df74d1cc8ec19acb6fe86c71abce7ec521
Franck Farcy <franck.farcy@gmail.com>
2025-07-17 19:30:26 +0200

    Revert to Tailwind v3 and add PDF reference

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <CinqueQueensPage
       coverSrc="/cinque_terre_bw.png"
       groupSrc="/group_photo_bw.png"
+      pdfHref="/Cinque_Queens_Weekend_Revised.pdf"
     />
   );
 }
